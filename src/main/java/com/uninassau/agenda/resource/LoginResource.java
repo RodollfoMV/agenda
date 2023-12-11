d12425b58d91fd3d5506ef4194cdb2a0d5874f0c
samueelM <samueel@gmail.com>
2023-12-10 22:05:36 -0300

    Conexão Login back->front

--- src/main/java/com/uninassau/agenda/resource/LoginResource.java
+++ src/main/java/com/uninassau/agenda/resource/LoginResource.java
@@ -2,6 +2,7 @@ package com.uninassau.agenda.resource;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ public class LoginResource {
 	}
 	
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping
 	public ResponseEntity<LoginDTO> login (@RequestBody LoginForm loginForm) {
 		
